216d4c9ad3edb06f34e8a335ea27741ec341fb3f
kevin alvino <kevinalvino167.com>
2026-05-15 21:23:33 +0700

    update

---
+++ resources/views/carousel.blade.php
@@ -0,0 +1,21 @@
+<div id="carouselExampleInterval" class="carousel slide mb-5" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280" class="d-block w-100" alt="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://awsimages.detik.net.id/community/media/visual/2026/03/29/kacau-motogp-amerika-2026-diguyur-penalti-marc-marquez-ikut-kena-1774753675524_169.jpeg?w=600&q=90" class="d-block w-100" alt="https://awsimages.detik.net.id/community/media/visual/2026/03/29/kacau-motogp-amerika-2026-diguyur-penalti-marc-marquez-ikut-kena-1774753675524_169.jpeg?w=600&q=90">
+    </div>
+    <div class="carousel-item">
+      <img src="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280" class="d-block w-100" alt="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
