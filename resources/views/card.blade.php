5f67857bbdb13a52839db48005aaea0b0bff645a
kevin alvino <kevinalvino167.com>
2026-05-16 18:54:59 +0700

    baru

--- resources/views/card.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="card" style="width: 18rem;">
-  <img src="https://www.crash.net/sites/default/files/2026-05/marc-marquez-2026-french-motogp.jpg?width=1600&aspect_ratio=16:9" class="card-img-top" alt="https://www.crash.net/sites/default/files/2026-05/marc-marquez-2026-french-motogp.jpg?width=1600&aspect_ratio=16:9">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
--- resources/views/layout/carousel.blade.php
+++ resources/views/layout/carousel.blade.php
@@ -1,20 +1,34 @@
-<div id="carouselExampleInterval" class="carousel slide mb-5" data-bs-ride="carousel">
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
+    <div class="carousel-item active">
       <img src="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280" class="d-block w-100" alt="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><a href="#">First slide label</a></h5>
+      </div>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
+    <div class="carousel-item">
       <img src="https://awsimages.detik.net.id/community/media/visual/2026/03/29/kacau-motogp-amerika-2026-diguyur-penalti-marc-marquez-ikut-kena-1774753675524_169.jpeg?w=600&q=90" class="d-block w-100" alt="https://awsimages.detik.net.id/community/media/visual/2026/03/29/kacau-motogp-amerika-2026-diguyur-penalti-marc-marquez-ikut-kena-1774753675524_169.jpeg?w=600&q=90">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><a href="#">Second slide label</a></h5>
+      </div>
     </div>
     <div class="carousel-item">
       <img src="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280" class="d-block w-100" alt="https://fr.alpinestars.com/cdn/shop/articles/MM93_3small.jpg?v=1744703478&width=1280">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><a href="#">Third slide label</a></h5>
+      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
